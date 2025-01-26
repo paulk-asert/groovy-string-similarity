c3012905c3735a8601881f7842cc0de2fa454ae9
Paul King <paulk@asert.com.au>
2025-01-26 22:16:16 +1000

    tweak sound example

--- src/main/groovy/Sound.groovy
+++ src/main/groovy/Sound.groovy
@@ -13,45 +13,68 @@ import static com.diogonunes.jcolor.Attribute.GREEN_TEXT
 var pairs = [
     ['cat', 'hat'],
     ['bear', 'bare'],
+    ['pair', 'pare'],
     ['there', 'their'],
+    ['sort', 'sought'],
     ['cow', 'bull'],
     ['winning', 'grinning'],
     ['knows', 'nose'],
     ['ground', 'aground'],
+    ['peeler', 'repeal'],
     ['hippo', 'hippopotamus'],
 ]
 
 var pretty = { a, b ->
-    var result = "$a|$b".padRight(22)
+    var result = "$a|$b".padRight(18)
     a == b ? colorize(result, GREEN_TEXT()) : result
 }
 
 var algs = [Soundex: { a, b -> new Soundex().with{pretty(soundex(a), soundex(b)) }},
     RefinedSoundex: { a, b -> new RefinedSoundex().with{ pretty(encode(a), encode(b)) }},
-    SoundexDiff: { a, b -> 4 - new Soundex().difference(a, b) },
-    Caverphone2: { a, b -> new Caverphone2().with{ pretty(encode(a), encode(b)) }},
     Metaphone: { a, b -> new Metaphone().with{ pretty(encode(a), encode(b)) }},
-    Metaphone8: { a, b -> new Metaphone(maxCodeLen: 8).with{ pretty(encode(a), encode(b)) }},
-    Metaphone8LCS: { a, b -> new Metaphone(maxCodeLen: 8).with{ new LongestCommonSubsequence().apply(encode(a), encode(b)) }},
-    Metaphone8Lev: { a, b -> new Metaphone(maxCodeLen: 8).with{ new LevenshteinDistance(8).apply(encode(a), encode(b)) }},
+    'Metaphone(6)': { a, b -> new Metaphone(maxCodeLen: 6).with{ pretty(encode(a), encode(b)) }},
     Metaphone3: { a, b -> new Metaphone3Keyer().with{ pretty(key(a), key(b)) }},
-    DoubleMetaphone: { a, b -> new DoubleMetaphone().with{ pretty(doubleMetaphone(a), doubleMetaphone(b)) }},
+    'DblMetaphone(6)': { a, b -> new DoubleMetaphone(maxCodeLen: 6).with{ pretty(doubleMetaphone(a), doubleMetaphone(b)) }},
+    Caverphone2: { a, b -> new Caverphone2().with{ pretty(encode(a), encode(b)) }},
+]
+
+var gameAlgs = [SoundexDiff: { a, b -> "${25 * new Soundex().difference(a, b)}%" },
+            Metaphone5LCS: { a, b ->
+                var (sa, sb) = new Metaphone(maxCodeLen: 5).with{ [encode(a), encode(b)] }
+                var max = [sa.size(), sb.size()].max()
+                var m = new LongestCommonSubsequence().apply(sa, sb)
+                "${(100 * m/max).round()}%" },
+            Metaphone5Lev: { a, b ->
+                var (sa, sb) = new Metaphone(maxCodeLen: 5).with{ [encode(a), encode(b)] }
+                var max = [sa.size(), sb.size()].max()
+                var m = max - new LevenshteinDistance(5).apply(sa, sb)
+                "${(100 / max * m).round()}%" },
 ]
 
 var results = [pairs, algs].combinations().collect { pair, namedAlg ->
     namedAlg.value(pair)
 }
 
-for (i in 0..algs.size()) {
-    if (i) print algs.entrySet()*.key[i-1].padRight(22)
-    else print 'Pair'.padRight(22)
-}
-println()
+display(algs, pairs, results, 18, 22)
 
-for (row in 0..<pairs.size()) {
+results = [pairs, gameAlgs].combinations().collect { pair, namedAlg ->
+    namedAlg.value(pair)
+}
+
+display(gameAlgs, pairs, results, 14, 24
+
+def display(algs, pairs, r, w1, w2) {
     for (i in 0..algs.size()) {
-        if (i) print "${results[(i-1) * pairs.size() + row]}".padRight(22)
-        else print pairs[row].join('|').padRight(22)
+        if (i) print algs.entrySet()*.key[i - 1].padRight(w1)
+        else print 'Pair'.padRight(w2)
     }
     println()
+
+    for (row in 0..<pairs.size()) {
+        for (i in 0..algs.size()) {
+            if (i) print "${r[(i - 1) * pairs.size() + row]}".padRight(w1)
+            else print pairs[row].join('|').padRight(w2)
+        }
+        println()
+    }
 }
