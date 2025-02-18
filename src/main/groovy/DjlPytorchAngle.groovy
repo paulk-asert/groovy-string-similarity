f5787ca26598d4f971f16e9da480841c1bfbca47
Paul King <paulk@asert.com.au>
2025-02-18 17:18:06 +1000

    angle tweaks

--- src/main/groovy/DjlPytorchAngle.groovy
+++ src/main/groovy/DjlPytorchAngle.groovy
@@ -4,25 +4,23 @@ import ai.djl.translate.DeferredTranslatorFactory
 
 import java.nio.file.Paths
 
-var phrases = [
-    'bull',
-    'bovine',
-    'kitten',
-    'hay',
-    'The sky is blue',
-    'The sea is blue',
-    'The grass is green',
-    'One two three',
-    'Bulls consume hay',
-    'Bovines convert grass to milk',
-    'Dogs play in the grass',
-    'Bulls trample grass',
-    'Dachshunds are delightful'
-]
+import static MathUtil.cosineSimilarity
 
 System.setProperty('org.slf4j.simpleLogger.defaultLogLevel', 'info')
 
-var path = Paths.get(DjlPytorchAngle.classLoader.getResource('UAE-Large-V1.zip').toURI())
+var samplePhrases = [
+    'bull', 'bovine', 'kitten', 'hay', 'The sky is blue',
+    'The sea is blue', 'The grass is green', 'One two three',
+    'Bulls consume hay', 'Bovines convert grass to milk',
+    'Dogs play in the grass', 'Bulls trample grass',
+    'Dachshunds are delightful', 'I like cats and dogs']
+
+var queries = [
+    'cow', 'cat', 'dog', 'grass', 'Cows eat grass',
+    'Poodles are cute', 'The water is turquoise']
+
+var modelName = 'UAE-Large-V1.zip'
+var path = Paths.get(DjlPytorchAngle.classLoader.getResource(modelName).toURI())
 var criteria = Criteria.builder()
     .setTypes(String, float[])
     .optModelPath(path)
@@ -32,14 +30,17 @@ var criteria = Criteria.builder()
 
 var model = criteria.loadModel()
 var predictor = model.newPredictor()
-var embeddings = phrases.collect(predictor::predict)
+var sampleEmbeddings = samplePhrases.collect(predictor::predict)
 
-['cow', 'cat', 'dog', 'grass', 'Cows eat grass',
- 'Poodles are cute', 'The water is turquoise'].each { query ->
+queries.each { query ->
     println "\n    $query"
-    var qe = predictor.predict(query)
-    var bestMatches = embeddings.collect { MathUtil.cosineSimilarity(it, qe) }.withIndex().sort { -it.v1 }.take(5)
-    bestMatches.each { printf '%s (%4.2f)%n', phrases[it.v2], it.v1 }
+    var queryEmbedding = predictor.predict(query)
+    sampleEmbeddings
+        .collect { cosineSimilarity(it, queryEmbedding) }
+        .withIndex()
+        .sort { -it.v1 }
+        .take(5)
+        .each { printf '%s (%4.2f)%n', samplePhrases[it.v2], it.v1 }
 }
 
 String[] words = ['cow', 'bull', 'calf', 'bovine', 'cattle', 'livestock', 'cat', 'kitten', 'feline',
@@ -47,6 +48,6 @@ String[] words = ['cow', 'bull', 'calf', 'bovine', 'cattle', 'livestock', 'cat',
 var wEmbeddings = words.collect{word -> predictor.predict(word) }
 wEmbeddings.eachWithIndex { s, i ->
     print "\n${words[i]}:"
-    var bestMatches = wEmbeddings.collect { MathUtil.cosineSimilarity(it, s) }.withIndex().findAll{ it.v2 != i }.sort { -it.v1 }.take(5)
+    var bestMatches = wEmbeddings.collect { cosineSimilarity(it, s) }.withIndex().findAll{ it.v2 != i }.sort { -it.v1 }.take(5)
     bestMatches.each { printf ' %s (%4.2f)', words[it.v2], it.v1 }
 }
